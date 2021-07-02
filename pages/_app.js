c60b19fba0f7b94327253741f97d095723e6007b
Hugo Pérard <hugo.perard@bearstudio.fr>
2021-07-02 18:00:14 +0200

    ADD FAVICON

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import 'tailwindcss/tailwind.css'
+import Head from "next/head";
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1e126e" />
+        <meta name="msapplication-TileColor" content="#1e126e" />
+        <meta name="theme-color" content="#1e126e" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
