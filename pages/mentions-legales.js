f3082b578c960b54fab282c00ec9474dc8dd1602
Yoann Fleury <yoann.fleury@yahoo.com>
2021-07-05 19:14:46 +0200

    feat: add mentions legales

--- pages/mentions-legales.js
+++ pages/mentions-legales.js
@@ -5,7 +5,32 @@ export default function MentionsLegales() {
   return (
     <>
       <Seo title="Mentions Légales" />
-      <Layout>Mentions légales</Layout>
+      <Layout>
+        <h2 className="text-4xl font-bold text-brand-600 text-center mt-8">
+          Mentions Légales
+        </h2>
+        <section>
+          <h3>Propriétaire</h3>
+          <p>
+            Le site TODO est édité par le BearStudio, SAS au capital social de
+            10 000 €
+          </p>
+          <p>
+            Inscrit au registre du commerce et des sociétés sous le numéro SIRET
+            TODO
+          </p>
+          <p>dont le siège social se situe au TODO.</p>
+          <p>Téléphone : 06 13 13 62 12 Mail : aurore.angevin@hotmail.fr</p>
+        </section>
+        <section>
+          <h3>Hébergeur</h3>
+          <p>
+            TODO est hébergé par Vercel Inc., dont le siège social se situe au
+            340 S Lemon Ave #4133 Walnut, CA 91789
+          </p>
+          <p>Site Internet : vercel.com</p>
+        </section>
+      </Layout>
     </>
   );
 }
